68cbdb14e20c5a8872d1dcf57dd1c6aabddcd070
Iddrisuabu <zaniddi@gmail.com>
2023-06-16 21:10:31 -0700

    lower

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description - Lower case Alphabets
+ * Return: 0
+ */
+
+int main(void)
+{
+	int x = 97;
+
+	while (x <= 122)
+	{
+		putchar(x);
+		x++;
+	}
+	putchar('\n');
+
+	return (0);
+}
