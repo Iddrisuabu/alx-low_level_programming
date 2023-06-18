01650b938e5ac9b460831cbba87840da49cb13ae
Iddrisuabu <zaniddi@gmail.com>
2023-06-18 09:20:51 -0700

    8-print_base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - entry piont
+ * description - hexadecimal
+ * Return: 0
+ */
+
+int main(void)
+{
+	int x;
+	int y;
+
+	for  (x = 48; x <= 57; x++)
+		putchar(x);
+
+	for (y = 97; y <= 102; y++)
+		putchar(y);
+	putchar('\n');
+
+	return (0);
+}
