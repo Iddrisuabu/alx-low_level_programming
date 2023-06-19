f6163e5b3d491eddce75f6a735ef6f4fccb230c0
Iddrisuabu <zaniddi@gmail.com>
2023-06-19 04:19:48 -0700

    100-print_comb3

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,14 +11,14 @@ int main(void)
 	int x;
 	int y;
 
-	for (x = 0; x <= 9; x++)
+	for (x = 0; x < 9; x++)
 	{
 		for (y = 0; y < 10; y++)
 		{
 			putchar(x + '0');
 			putchar(y + '0');
 
-			if (x == 9 && y == 9)
+			if (x == 8 && y == 9)
 				continue;
 
 			putchar(',');
