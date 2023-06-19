febdbae6e0d980d16ddab43c1c8e4a8017b26bee
Iddrisuabu <zaniddi@gmail.com>
2023-06-18 17:26:06 -0700

    100-print_comb3

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - enrty point.
+ * Description - possible different combination of two digit.
+ * Return: 0
+ */
+
+int main(void)
+{
+	int x;
+	int y;
+
+	for (x = 0; x <= 9; x++)
+	{
+		for (y = 0; y < 10; y++)
+		{
+			putchar(x + '0');
+			putchar(y + '0');
+
+			if (x == 9 && y == 9)
+				continue;
+
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
