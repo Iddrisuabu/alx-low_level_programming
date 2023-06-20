dfcdeae22472284926ee8f151e3a6d12df9077f5
Iddrisuabu <zaniddi@gmail.com>
2023-06-20 08:19:56 -0700

    0-putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -12,9 +12,11 @@ int main(void)
 	_putchar('p');
 	_putchar('u');
 	_putchar('t');
-	-putchar('c');
+	_putchar('c');
 	_putchar('h');
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
+
+	return (0);
 }
