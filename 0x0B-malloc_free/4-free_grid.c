5184797cce4a31bec4c4e33a646a20d23c5bd8ba
Iddrisuabu <zaniddi@gmail.com>
2023-07-12 13:17:16 -0700

    free

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -2,15 +2,9 @@
 #include <stdlib.h>
 
 /**
-<<<<<<< HEAD
  * free_grid - to free allocated memory to an array.
  * @height: height of array
  * @grid: array to be freed.
-=======
- * free_grid - to free allocated memory to an array
- * @grid: array to be freed.
- * @height: height of the array.
->>>>>>> 03b866dc3a63273f64d2b08b132b0fb4795bd204
  * Return: void.
  */
 
@@ -24,9 +18,6 @@ void free_grid(int **grid, int height)
 		y++;
 	}
 
-<<<<<<< HEAD
 	free(grid);
 
-=======
->>>>>>> 03b866dc3a63273f64d2b08b132b0fb4795bd204
 }
