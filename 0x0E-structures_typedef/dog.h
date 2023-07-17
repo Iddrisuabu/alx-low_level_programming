cfa90f23ca22d7491b48b3aeb443e2ecb4903aec
Iddrisuabu <zaniddi@gmail.com>
2023-07-17 10:42:19 -0700

    dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef dog_h
+#define dog_h
+
+/**
+ * struct dog - details of dog.
+ * @name: name of dog.
+ * @age: age of dog.
+ * @owner: owner of dog.
+ *
+ * Description: long description.
+ */
+
+struct dog
+{
+	char *name;
+	float *age;
+	char *owner;
+};
+
+#endif
