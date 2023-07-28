ddd07ce10871c4d58e4bced1772a5c2a54ad3d86
Iddrisuabu <zaniddi@gmail.com>
2023-07-28 14:32:17 -0700

    first

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * _print_str - function that prints a string.
+ * Return: 0.
+ */
+
+void _print_str(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+
+}
